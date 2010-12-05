17caf27639aea9d99e4afc17dc5ee3b266e60181
Irakli Gozalishvili <rfobic@gmail.com>
2010-12-05 19:56:27 +0100

    Adding unit test for all the errors that had been fixed by the last  batch of commits.

--- test/all.js
+++ test/all.js
@@ -6,6 +6,7 @@ exports['test asap'] = require('./asap');
 exports['test rosolve & reject'] = require('./resolve-reject')
 exports['test chains of promises'] = require('./promised-chains')
 exports['test multiple listeners'] = require('./multiple-listeners')
+exports['test methods'] = require('./methods')
 
 if (module == require.main) require('test').run(exports)
 
