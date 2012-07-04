cd5d3c42cda6a560b4433c89d92d9cdd05b27882
Domenic Denicola <domenic@domenicdenicola.com>
2012-07-03 20:13:52 -0400

    Fix the #73 regression test.
    It wasn't prepared for cross-origin restrictions (which happen on the local file system with Chrome), and seems to have had a logic error in the Node case that was masked by the problem fixed in the previous commit.

--- spec/q-spec.js
+++ spec/q-spec.js
@@ -1028,7 +1028,10 @@ describe("possible regressions", function () {
             var deferred = Q.defer();
 
             function checkErrorMessage(message) {
-                if (message.indexOf(REASON) === -1) {
+                // See http://stackoverflow.com/questions/5913978/cryptic-script-error-reported-in-javascript-in-chrome-and-firefox
+                // for an explanation of why we check for "Script error."
+                if (message.indexOf(REASON) === -1 &&
+                    message !== "Script error.") {
                     deferred.reject(new Error(
                         "Error was thrown when calling .end(): " + message
                     ));
@@ -1038,8 +1041,8 @@ describe("possible regressions", function () {
             if (typeof window !== "undefined") {
                 window.onerror = checkErrorMessage;
             } else if (typeof process !== "undefined") {
-                process.on("uncaughtException", function (exception) {
-                    checkErrorMessage(exception.message);
+                process.on("uncaughtException", function (thrown) {
+                    checkErrorMessage(thrown);
                 });
             }
 
