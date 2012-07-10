d18cea5028d1ef7c820084abb40d3d86157aa01f
Brian Mitchell <brian@generalassemb.ly>
2012-07-10 12:52:26 -0300

    We probably want to return the object not the value set for the case of `put(key, value)` so that we can continue working with that value down the chain.

--- q.js
+++ q.js
@@ -744,7 +744,8 @@ function resolve(object) {
             return object[name];
         },
         "put": function (name, value) {
-            return object[name] = value;
+            object[name] = value;
+            return object;
         },
         "del": function (name) {
             return delete object[name];
