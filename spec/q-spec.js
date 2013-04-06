cfcffff27018f13111b92915a50c5f224bbac158
Andrew C. Greenberg <wizardwerdna@gmail.com>
2013-04-05 23:58:07 -0700

    Failing test in Node.js 0.10 for the nextTick issue.

--- spec/q-spec.js
+++ spec/q-spec.js
@@ -19,6 +19,27 @@ afterEach(function () {
     Q.onerror = null;
 });
 
+describe("computing sum of integers using promises", function() {
+   it("should compute correct result without blowing stack", function () {
+      var array = [];
+      var iters = 1000;
+      for (var i=1; i<=iters; i++){
+       array.push(i)
+      }
+      var pZero = Q.fulfill(0);
+      var result = array.reduce(function(promise, nextVal) {
+         return promise.then(function(currentVal) {
+            var pNext = Q.fulfill(currentVal + nextVal);
+            return pNext;
+         });
+      }, pZero);
+      result.then(function(value){
+         expect(value).toEqual(iters*(iters+1)/2);
+         done();
+      });
+   });
+});
+
 describe("Q function", function () {
     it("should result in a fulfilled promise when given a value", function () {
         expect(Q(5).isFulfilled()).toBe(true);
