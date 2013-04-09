d144e138e29565aaf8dbc054be2f7f13dda274d5
Mark S. Miller <erights@gmail.com>
2013-04-08 19:37:31 -0700

    damn tabs

--- q.js
+++ q.js
@@ -649,9 +649,9 @@ Q.join = join;
 function join(x, y) {
     return Q.all([x, y]).spread(function(x, y) {
         if (x === y) {
-	    // TODO: "===" should be Object.is or equiv
-	    return x;
-	}
+            // TODO: "===" should be Object.is or equiv
+            return x;
+        }
         throw new Error("not the same");
     });
 }
@@ -665,8 +665,8 @@ function race(answerPs) {
 //        });
 //        Use this in the meantime
           for (var i = 0, len = answerPs.length; i < len; i++) {
-	      Q(answerPs[i]).then(resolve,reject);
-	  }
+              Q(answerPs[i]).then(resolve,reject);
+          }
     });
 }
 
