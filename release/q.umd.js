3b2e483e02c993065044b01830acb7d42b97149c
Domenic Denicola <domenic@domenicdenicola.com>
2013-06-11 20:55:38 -0400

    Use browserify to build from CommonJS.
    - q.js in the root is now in CommonJS format.
    - Running `npm run prepublish` will generate a release folder containing q.umd.js and q.min.js.
    - q.umd.js is a "Universal Module Definition" version generate by browserify's "standalone" option, with support for the many module systems and loaders.
    - q.min.js is the minified version of q.umd.js.
    - Also cleaned up grunt syntax to be less verbose.

--- release/q.umd.js
+++ release/q.umd.js
@@ -1,3 +1,5 @@
+(function(e){if("function"==typeof bootstrap)bootstrap("q",e);else if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else if("undefined"!=typeof ses){if(!ses.ok())return;ses.makeQ=e}else"undefined"!=typeof window?window.Q=e():global.Q=e()})(function(){var define,ses,bootstrap,module,exports;
+return (function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
 // vim:ts=4:sts=4:sw=4:
 /*!
  *
@@ -1718,3 +1720,7 @@ function nodeify(promise, nodeback) {
 var qEndingLine = captureLine();
 
 module.exports = Q;
+
+},{}]},{},[1])(1)
+});
+;
