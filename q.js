dc92d37b55405841482aeddc6b9e6b5a95dffa88
Martin Zagora <mzagora@gratex.com>
2013-09-16 13:26:51 +1000

    Clarify need for stronger CommonJS boilerplate

--- q.js
+++ q.js
@@ -40,7 +40,13 @@
         bootstrap("promise", definition);
 
     // CommonJS
-    } else if (typeof module !== "undefined" && typeof exports === "object") {
+    // The existence `require`, `exports`, *and* `object` in scope strongly
+    // implies that this is a CommonJS environment, as opposed to a RequireJS
+    // environment which would have `require` and `define`.  This must be
+    // specific enough to eliminate the possibility that this is being run by
+    // Adobe Brackets, which provides global `require` and `exports`, but not
+    // `module`.
+    } else if (typeof module === "object" && typeof exports === "object") {
         module.exports = definition();
 
     // RequireJS
