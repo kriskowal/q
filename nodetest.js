c79dc03d0251fb45faac89a9bb9734db484c7de2
Benjamin Gruenbaum <Benjamin@Benjamins-Macbook-Air.local>
2015-02-13 16:02:44 +0200

    the amount of fail I have with git today is spectacular

--- nodetest.js
+++ nodetest.js
@@ -3,5 +3,5 @@ var q = require("./q/index.js");
 q.reject("Heh");
 
 process.on("unhandledRejection", function(handler, reason){
-	console.log("Found it!", handler, reason);
+	console.log("Found it!", handler, "reason", reason);
 });
