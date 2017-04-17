f0d4170f86cbfb5167f79f8e809f86608d01e425
Marcel Schnirring <ms@marcel-schnirring.de>
2017-04-17 20:24:58 +0200

    added initial notification

--- q.js
+++ q.js
@@ -1610,6 +1610,8 @@ function all(promises) {
         }, void 0);
         if (pendingCount === 0) {
             deferred.resolve(promises);
+        } else {
+            deferred.notify({ resolved: promises.length-pendingCount, pending: pendingCount, total: promises.length });
         }
         return deferred.promise;
     });
